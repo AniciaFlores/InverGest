80dba7085c808c5e68174945c74d25504b438d03
anicia_flores <redactorweboficial@gmail.com>
2024-04-22 19:49:29 -0300

    Pág FAQ, link de contacto, formulario de opinion

--- faq.js
+++ faq.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function() {
-    var button = document.querySelector(".back-to-top");
+    let button = document.querySelector(".back-to-top");
     if (button) {
         if (window.pageYOffset > 100) {
             button.style.display = "block";
@@ -9,3 +9,19 @@ window.addEventListener("scroll", function() {
     }
 });
 
+document.getElementById('feedback-form').addEventListener('submit', function(event) {
+    // Esto previene la recarga de la página
+    event.preventDefault();
+
+    // Obtén el texto ingresado por el usuario
+    let feedback = document.getElementById('feedback').value;
+
+    // Verifica si el usuario ingresó algún texto
+    if (feedback.trim() !== '') {
+        // Si el usuario ingresó algún texto, muestra el mensaje de agradecimiento
+        document.getElementById('thankyou-message').style.display = 'block';
+    } else {
+        // Si el usuario no ingresó ningún texto, puedes mostrar un mensaje de error o hacer algo más
+        alert('Por favor, ingresa tu opinión antes de enviar.');
+    }
+});
